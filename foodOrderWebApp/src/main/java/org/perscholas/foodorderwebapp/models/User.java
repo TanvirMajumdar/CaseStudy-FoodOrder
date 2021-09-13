47f75f5cae121ab4160419872a357181a33bfb56
Tanvir Majumdar <tanvir.majumdar.03@gmail.com>
2021-09-12 21:45:50 -0500

    updated project

--- foodOrderWebApp/src/main/java/org/perscholas/foodorderwebapp/models/User.java
+++ foodOrderWebApp/src/main/java/org/perscholas/foodorderwebapp/models/User.java
@@ -48,7 +48,7 @@ public class User {
 
 
     @NonNull
-    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "users_roles",
             joinColumns = @JoinColumn(name = "user_id"),
